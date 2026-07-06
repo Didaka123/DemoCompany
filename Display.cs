9ae02099c4ea8f04af291d24940ca80205bc6321
GRIGS <GRIGS@DESKTOP-7R2C1CL>
2026-07-06 23:24:30 +0300

    Add project files.

---
+++ Display.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoCompany
+{
+    internal class Display
+    {
+       public static void Menu ()
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("\n=== Моля изберете ===");
+            Console.ResetColor();
+
+            Console.WriteLine("1) Добави нов служител");
+            Console.WriteLine("2) Редактирай данни за служител");
+            Console.WriteLine("3) Справка за активните служители");
+            Console.WriteLine("4) Справка за напусналите служители ");
+            Console.WriteLine("5) Преминаване в нова година ");
+
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Write("Вашият избор:");
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            int choose = int.Parse(Console.ReadLine());
+            Console.ResetColor();
+
+            switch (choose)
+            {
+                case 1: Worker.AddNewWorker(); break;
+                case 2: Worker.EditWorkerData(); break;
+                case 3: Worker.(); break;
+                default: Environment.Exit(0); break;
+            }
+            Menu();
+        }
+    }
+}
---
+++ Worker.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoCompany
+{
+    internal class Worker
+    {
+        public static List<Worker> Workers = new List<Worker>();
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public int Age { get; set; }
+        public int Internship { get; set; }
+        public int Salary { get; set; }
+        public string Position { get; set; }
+
+
+        public Worker(int id, string name, string family, int age, int internship, int salary, string position)
+        {
+            Id = id;
+            Name = name;
+            Family = family;
+            Age = age;
+            Internship = internship;
+            Salary = salary;
+            Position = position;
+        }
+
+        public static void AddNewWorker()
+        {
+
+        }
+
+        public static void EditWorkersData()
+        {
+
+        }
+
+        public static void ActiveWorkersReport()
+        {
+        }
+
+        public static void InactiveWorkersReport()
+        {
+        }
+    }
+}
